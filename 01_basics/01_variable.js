24b93f75297086563a505b6932247ff4bd411a7c
Chetan728 <158229327+Chetan728@users.noreply.github.com>
2024-03-29 14:39:27 +0000

    datatype and variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 123456
+let accountEmail = "chetandhakar1234@gmail.com"
+var accountPassword = "9516372969"
+accountCity = "Bhopal"
+let accountState=" ";
+
+// use late variable to define not use var 
+
+// accountId = 12345  // not allowed 
+
+accountEmail="chetan@gamil.com"
+accountPassword="1234"
+accountCity="morena"
+console.log(accountEmail);
+console.table([accountId,accountEmail, accountPassword , accountCity, accountState])
