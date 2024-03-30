038f1976bf8b31cad27de806991e5c014017d0e2
Chetan728 <158229327+Chetan728@users.noreply.github.com>
2024-03-30 15:13:50 +0000

    learn new

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name="hitesh"
+const repoCount = 50 
+
+// console.log(name + repocount+ " Value"); // This is wrong writing code 
+console.log(`Helo My name is ${name} and i have repo${repoCount}`);
+
+const gameName= new String(' hitesh-hc-com')
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('t'));
+console.log(gameName.substring(0, 5));
+console.log(gameName.slice(-8, 4));
+console.log(gameName.trim());
+console.log(gameName.split('-'));
+
+// there are lot of methods for string for different purposes so please practice methods and get reference form mdn documention
---
+++ 01_basics/memory.js
@@ -0,0 +1,19 @@
+// stack - primitive , heap - nonprimitive 
+let myphone="9516372969"
+ let changePhone=myphone
+ changePhone="7828755473"
+//  console.log(myphone);
+//  console.log(changePhone);
+
+let userOne= {
+    email: "user@googgle.com",
+    upi: "user@ybl"
+}
+
+let userTwo=userOne
+userTwo.email="chetan@google.com"
+
+console.log(userOne.email);
+
+
+
