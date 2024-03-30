06cda4cbf76f1040938169eb4fb1df7817b703d4
Chetan728 <158229327+Chetan728@users.noreply.github.com>
2024-03-30 19:16:56 +0000

    number and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+const score = 400 
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+
+const otherNumber =123.845
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-In'))
+
+
+/* so yaha par meri number finish hoti , yaha maine different purpose k liye use hone wale number k different 
+numberes k method dhekhe , aur number likhene k do tareeek hote h . 
+*/
+
+// *********** MATHS **************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.1));
+// console.log(Math.min(1,4,5));
+// console.log(Math.max(2,4,7));
+
+console.log(Math.random()); // generate 0 to 1 value
+console.log(Math.random()*10 + 1) // generate 1 to 10 
+console.log(Math.floor(Math.random()*10 + 1)) // generate integer 1 to 10 
+
+const min = 10 
+const max = 12
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min)
+
+// remeber the formula ( max - min + 1)
+
+// doubt k min aur max k beech ki value chahiye leking min aur max nahi aani chahiye to use kaise karenge 
+
+
