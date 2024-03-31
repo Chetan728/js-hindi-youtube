ec55441aa0c4d94f0de7e906f4cb7a237db8edbf
Chetan728 <158229327+Chetan728@users.noreply.github.com>
2024-03-31 19:19:52 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+// array 
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array (1,2,3,4)
+// console.log(myArr[1]);
+
+// Array method 
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+// console.log(myArr);
+
+myArr.unshift(9)
+// console.log(myArr);
+
+myArr.shift()
+// console.log(myArr)
+
+// console.log(myArr.includes(12)); // data type is boolean 
+// console.log(myArr.indexOf(15));
+// console.log(myArr.indexOf(4));
+
+// console.log(myArr.join()); // convert to array to string 
+
+// slice , splice 
+
+console.log("A" , myArr);
+console.log("B", myArr.slice(1,3)) // end not include 
+console.log("c", myArr.splice(1,3)) // break in two array include end 
+console.log(myArr) 
+
+
+
+
