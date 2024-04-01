f4d7f541790cbb4a6359393f92d7317b398a7b03
Chetan728 <158229327+Chetan728@users.noreply.github.com>
2024-04-01 18:16:43 +0000

    object desturcturing and api

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,4 +1,4 @@
-// learning - skeloton , literal ,, Object.assign() , ... ,,  keys , value , entries , hasOwnProperty  
+// learning - skeloton , literal ,, Object.assign() , ... ,, v,  keys alue , entries , hasOwnProperty  
 // const tinderUser = new Object() // skeloton 
 
 const tinderUser = {}
@@ -51,12 +51,42 @@ const users = [
 
 // console.log(users[1].email);
 
-console.log(tinderUser)
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isloggedIn'));
+// console.log(tinderUser.hasOwnProperty('isloggedIn'));
+
+
+// object destructureing 
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+// api json structure 
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
 
 
 
